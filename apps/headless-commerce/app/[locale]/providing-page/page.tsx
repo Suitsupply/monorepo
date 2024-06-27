085e94708608d99fc49b7f230af40b58d931c497
ss-twieland <twieland@suitsupply.com>
2024-06-27 14:48:50 +0200

    feat(apps/headless-commerce): #HEAD-0000: Put providing page in separate page (#11)
    Co-authored-by: Tom Wieland <tom.wieland@gmail.com>

---
+++ apps/headless-commerce/app/[locale]/providing-page/page.tsx
@@ -0,0 +1,9 @@
+import ProvidingIndexPage from '@susu/providing-application/src/app/page';
+
+export default function ProvidingPage() {
+  return (
+    <div>
+      <ProvidingIndexPage />
+    </div>
+  );
+}
